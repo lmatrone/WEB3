3e60e69e7e01dfdebb8045f25802a2b7ec6212b0
Leonardo Matrone <50384620+lmatrone@users.noreply.github.com>
2019-06-11 20:32:28 -0300

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,101 +1,100 @@
 const   contractABI = [
-    [
-        {
-            "constant": false,
-            "inputs": [
-                {
-                    "name": "paramEndereco",
-                    "type": "address"
-                },
-                {
-                    "name": "paramNomeProprietario",
-                    "type": "string"
-                },
-                {
-                    "name": "paramValorVenal",
-                    "type": "uint256"
-                }
-            ],
-            "name": "registraImovel",
-            "outputs": [],
-            "payable": false,
-            "stateMutability": "nonpayable",
-            "type": "function"
-        },
-        {
-            "constant": true,
-            "inputs": [
-                {
-                    "name": "matricula",
-                    "type": "uint256"
-                }
-            ],
-            "name": "devolveNomeProprietario",
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "string"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "constant": true,
-            "inputs": [
-                {
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "name": "livro1",
-            "outputs": [
-                {
-                    "name": "endereco",
-                    "type": "address"
-                },
-                {
-                    "name": "nomeProprietario",
-                    "type": "string"
-                },
-                {
-                    "name": "valorVenal",
-                    "type": "uint256"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "constant": true,
-            "inputs": [
-                {
-                    "name": "",
-                    "type": "address"
-                }
-            ],
-            "name": "livro2",
-            "outputs": [
-                {
-                    "name": "endereco",
-                    "type": "address"
-                },
-                {
-                    "name": "nomeProprietario",
-                    "type": "string"
-                },
-                {
-                    "name": "valorVenal",
-                    "type": "uint256"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
-        }
-    ]
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "paramEndereco",
+				"type": "address"
+			},
+			{
+				"name": "paramNomeProprietario",
+				"type": "string"
+			},
+			{
+				"name": "paramValorVenal",
+				"type": "uint256"
+			}
+		],
+		"name": "registraImovel",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "matricula",
+				"type": "uint256"
+			}
+		],
+		"name": "devolveNomeProprietario",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "livro1",
+		"outputs": [
+			{
+				"name": "endereco",
+				"type": "address"
+			},
+			{
+				"name": "nomeProprietario",
+				"type": "string"
+			},
+			{
+				"name": "valorVenal",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "livro2",
+		"outputs": [
+			{
+				"name": "endereco",
+				"type": "address"
+			},
+			{
+				"name": "nomeProprietario",
+				"type": "string"
+			},
+			{
+				"name": "valorVenal",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
     
     if (network === "4") {
         contractAddress = "0xe54b2739e2b54a48c5e6f46ffef2dbbfb3d31358"; 
